192e163645ebf3bdf314727d28fb9a16a893e3bd
kvale1733 <kevin.valencia@liferay.com>
2023-03-01 07:40:17 -0800

    Revert "LPS-176197 allow data to be keep format when rendered"
    This reverts commit c731807b7d9e9584ce68b4fe7ec22745dba1c539.

--- modules/apps/change-tracking/change-tracking-spi/src/main/java/com/liferay/change/tracking/spi/display/BaseCTDisplayRenderer.java
+++ modules/apps/change-tracking/change-tracking-spi/src/main/java/com/liferay/change/tracking/spi/display/BaseCTDisplayRenderer.java
@@ -166,27 +166,11 @@ public abstract class BaseCTDisplayRenderer<T extends BaseModel<T>>
 		public DisplayBuilder<T> display(String languageKey, Object value);
 
 		public DisplayBuilder<T> display(
-			String languageKey, Object value, boolean escape);
-
-		public DisplayBuilder<T> display(
-			String languageKey, Object value, boolean escape,
-			boolean formatted);
-
-		public DisplayBuilder<T> display(
 			String languageKey, String value, boolean escape);
 
 		public DisplayBuilder<T> display(
 			String languageKey,
 			UnsafeSupplier<Object, Exception> unsafeSupplier);
-
-		public DisplayBuilder<T> display(
-			String languageKey,
-			UnsafeSupplier<Object, Exception> unsafeSupplier, boolean escape);
-
-		public DisplayBuilder<T> display(
-			String languageKey,
-			UnsafeSupplier<Object, Exception> unsafeSupplier, boolean escape,
-			boolean formatted);
 
 		public DisplayContext<T> getDisplayContext();
 
@@ -203,21 +187,6 @@ public abstract class BaseCTDisplayRenderer<T extends BaseModel<T>>
 
 		@Override
 		public DisplayBuilder<T> display(String languageKey, Object value) {
-			return display(languageKey, value, true);
-		}
-
-		@Override
-		public DisplayBuilder<T> display(
-			String languageKey, Object value, boolean escape) {
-
-			return display(languageKey, value, escape, false);
-		}
-
-		@Override
-		public DisplayBuilder<T> display(
-			String languageKey, Object value, boolean escape,
-			boolean formatted) {
-
 			HttpServletResponse httpServletResponse =
 				_displayContext.getHttpServletResponse();
 
@@ -228,10 +197,6 @@ public abstract class BaseCTDisplayRenderer<T extends BaseModel<T>>
 				writer.write("table-cell-expand-small\">");
 				writer.write(LanguageUtil.get(_resourceBundle, languageKey));
 				writer.write("</td><td class=\"table-cell-expand\">");
-
-				if (formatted) {
-					writer.write("<pre>");
-				}
 
 				if (value instanceof Blob) {
 					String downloadURL = _displayContext.getDownloadURL(
@@ -258,16 +223,7 @@ public abstract class BaseCTDisplayRenderer<T extends BaseModel<T>>
 					writer.write(format.format(value));
 				}
 				else {
-					if (escape) {
-						writer.write(HtmlUtil.escape(String.valueOf(value)));
-					}
-					else {
-						writer.write(String.valueOf(value));
-					}
-				}
-
-				if (formatted) {
-					writer.write("</pre>");
+					writer.write(HtmlUtil.escape(String.valueOf(value)));
 				}
 
 				writer.write("</td></tr>");
@@ -283,7 +239,30 @@ public abstract class BaseCTDisplayRenderer<T extends BaseModel<T>>
 		public DisplayBuilder<T> display(
 			String languageKey, String value, boolean escape) {
 
-			return display(languageKey, value, escape, false);
+			HttpServletResponse httpServletResponse =
+				_displayContext.getHttpServletResponse();
+
+			try {
+				Writer writer = httpServletResponse.getWriter();
+
+				writer.write("<tr><td class=\"publications-key-td ");
+				writer.write("table-cell-expand-small\">");
+				writer.write(LanguageUtil.get(_resourceBundle, languageKey));
+				writer.write("</td><td class=\"table-cell-expand\">");
+
+				if (escape) {
+					value = HtmlUtil.escape(value);
+				}
+
+				writer.write(value);
+
+				writer.write("</td></tr>");
+			}
+			catch (IOException ioException) {
+				throw new UncheckedIOException(ioException);
+			}
+
+			return this;
 		}
 
 		@Override
@@ -291,28 +270,11 @@ public abstract class BaseCTDisplayRenderer<T extends BaseModel<T>>
 			String languageKey,
 			UnsafeSupplier<Object, Exception> unsafeSupplier) {
 
-			return display(languageKey, unsafeSupplier, true);
-		}
-
-		@Override
-		public DisplayBuilder<T> display(
-			String languageKey,
-			UnsafeSupplier<Object, Exception> unsafeSupplier, boolean escape) {
-
-			return display(languageKey, unsafeSupplier, escape, false);
-		}
-
-		@Override
-		public DisplayBuilder<T> display(
-			String languageKey,
-			UnsafeSupplier<Object, Exception> unsafeSupplier, boolean escape,
-			boolean formatted) {
-
 			try {
 				Object value = unsafeSupplier.get();
 
 				if (value != null) {
-					display(languageKey, value, escape, formatted);
+					display(languageKey, value);
 				}
 			}
 			catch (Exception exception) {
