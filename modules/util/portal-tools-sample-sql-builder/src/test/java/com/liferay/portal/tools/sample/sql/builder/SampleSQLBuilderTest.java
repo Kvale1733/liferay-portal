b5bd76483b03206141c26f2afd9dfd4760477996
kvale1733 <kevin.valencia@liferay.com>
2023-04-24 12:32:54 -0700

    Revert "LPS-180672 Simplify"
    This reverts commit 2cca755d4d0004504dd64da4e03d9ad76e2509ee.

--- modules/util/portal-tools-sample-sql-builder/src/test/java/com/liferay/portal/tools/sample/sql/builder/SampleSQLBuilderTest.java
+++ modules/util/portal-tools-sample-sql-builder/src/test/java/com/liferay/portal/tools/sample/sql/builder/SampleSQLBuilderTest.java
@@ -16,7 +16,9 @@ package com.liferay.portal.tools.sample.sql.builder;
 
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.dao.db.DBManagerImpl;
 import com.liferay.portal.kernel.dao.db.DB;
+import com.liferay.portal.kernel.dao.db.DBManager;
 import com.liferay.portal.kernel.dao.db.DBManagerUtil;
 import com.liferay.portal.kernel.dao.db.DBType;
 import com.liferay.portal.kernel.util.FileUtil;
@@ -198,7 +200,9 @@ public class SampleSQLBuilderTest {
 	private void _loadHypersonic(Connection connection, String fileName)
 		throws Exception {
 
-		DB db = DBManagerUtil.getDB();
+		DBManager dbManager = new DBManagerImpl();
+
+		DB db = dbManager.getDB(DBType.HYPERSONIC, null);
 
 		List<String> lines = Files.readAllLines(
 			Paths.get(fileName), StandardCharsets.UTF_8);
