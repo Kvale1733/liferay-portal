5086e09156b62e71a30d19c7046f5bc3bbfea071
Kevin Valencia <kevin.valencia@liferay.com>
2023-11-30 11:59:30 -0800

    Revert "LPS-197921 Moves InfoFormException to portal-kernel"
    This reverts commit 5178929b66fbe30176bd131b37621084622ee74e.

--- modules/apps/info/info-api/src/main/java/com/liferay/info/exception/InfoFormException.java
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/exception/InfoFormException.java
@@ -3,8 +3,9 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.exception;
+package com.liferay.info.exception;
 
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
 
 import java.util.Locale;
